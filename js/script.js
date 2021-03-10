ff5be2761a781f55847dbcdabff3a99a39b19d84
gil <gilpaskhover98@gmail.com>
2021-03-10 18:39:48 +0200

    Done the logic of the game

--- js/script.js
+++ js/script.js
@@ -1,47 +1,86 @@
-const symbols = ["rock", "paper", "scissors"];
-            function computerPlay(){
-                let randIndex = Math.floor(Math.random() * 3);
-                return symbols[randIndex];
-            }
-            function playRound(playerSelection, computerSelection){
-                playerSelection = playerSelection.toLowerCase();
+const SYMBOLS = ["rock", "paper", "scissors"];
+//Generate random symbol for computer's choice
+function computerPlay(){
+    let randIndex = Math.floor(Math.random() * 3);
+    let symbol = SYMBOLS[randIndex];
+    const computerPhoto = document.querySelector("img[name='cp']");
+    computerPhoto.src = `/pictures/${symbol}.png`;
+    return symbol;
+}
+//check who is the winner according to the choices
+function checkWinner(player, computer){
+    if (playerSelection === computerSelection) return "tie"; // tie
 
-                if (playerSelection === computerSelection) return 2; // tie
+    if (playerSelection === "rock"){
+        if (computerSelection === "paper") return "computer"; //computer wins
+        if (computerSelection === "scissors") return "player"; //player wins
+    }
+    if (playerSelection === "paper"){
+        if (computerSelection === "rock") return "player";
+        if (computerSelection === "scissors") return "computer";
+    }
+    if (playerSelection === "scissors"){
+        if (computerSelection === "rock") return "computer";
+        if (computerSelection === "paper") return "player";
+    }
+    // default return value
+    return -1;
+}
+//updates the paragraph that describes what are the selections
+function changePSelections(pselect, cselect){
+        const para = document.querySelector("p[id='selections']");
+        para.textContent = `${pselect} vs ${cselect}`;
+}
+//updates the score and plays the equivilent sound
+function changeScore(winner){
+    const pwinner = document.querySelector("p[id='winner']");
+    let audioSelect = undefined;
+    if (winner === "player"){
+        wins++;
+        pwinner.textContent = "You Won!";
+        audioSelect = document.querySelector("audio[data-key='win']");
+    } 
+    if (winner === "computer"){ 
+        losses++;
+        pwinner.textContent = "You Lost!";
+        audioSelect = document.querySelector("audio[data-key='loss']");
+    }
+    if (winner === "tie"){
+        pwinner.textContent = "It's a Tie!"
+        audioSelect = document.querySelector("audio[data-key='tie']");
+    }
+    const pscore = document.querySelector("p[id='score-board']");
+    pscore.textContent = `${wins} - ${losses}`;
 
-                if (playerSelection === "rock"){
-                    if (computerSelection === "paper") return 0; //computer wins
-                    if (computerSelection === "scissors") return 1; //player wins
-                }
-                if (playerSelection === "paper"){
-                    if (computerSelection === "rock") return 1;
-                    if (computerSelection === "scissors") return 0;
-                }
-                if (playerSelection === "scissors"){
-                    if (computerSelection === "rock") return 0;
-                    if (computerSelection === "paper") return 1;
-                }
+    //playing music
+    audioSelect.currentTime = 0;
+    audioSelect.play();
+}
+//workflow of the whole game
+function playRound(element){
+    playerSelection = element.target.name;
+    computerSelection = computerPlay();
+    changePSelections(playerSelection, computerSelection);
+    let winner = checkWinner(playerSelection, computerSelection);
+    changeScore(winner);
+}
+//resets the game
+function reset(){
+    wins = 0, losses = 0;
+    const pscore = document.querySelector("p[id='score-board']");
+    const pselect = document.querySelector("p[id='selections']");
+    const pwinner = document.querySelector("p[id='winner']");
+    pscore.textContent = "0 - 0";
+    pselect.textContent = "Choose Wisely";
+    pwinner.textContent = "(click the picture)";
+}
 
-                // default return value if player didn't entered a valid input
-                return -1;
-            }
-            function game(){
-                let playerWins = 0;
-                const numberOfGames = 5;
-                for(let i = 1; i <= numberOfGames; i++){
-                    let playerChoice = prompt("Enter your choise: Rock, Paper or Scissors!");
-                    let computerChoice = computerPlay();
-                    console.log(`Player selected: ${playerChoice} and the computer selected: ${computerChoice}.`)
+let wins = 0, losses = 0;
+//adding eventlisteneres for the player choices
+const photos = document.querySelectorAll(".photo");
+photos.forEach((photo)=>{
+    photo.addEventListener("click", playRound);
+})
 
-                    let result = playRound(playerChoice, computerChoice);
-                    switch(result){
-                        case 0: console.log("You lost!");break;
-                        case 1: console.log("You won!"); playerWins++; break;
-                        case 2: console.log("It's a tie!"); i--; break;
-                    }
-                }
-
-                console.log(`You have ${playerWins} and the computer has ${numberOfGames - playerWins}.`);
-                console.log(`${playerWins > 3?"You Won The MATCH!!!":"You Lost The MATCH!!!"}`);
-            }
-
-            console.log(playRound("rock", "paper"));
+const btn = document.querySelector("button");
+btn.addEventListener('click', reset);
